31ce7ce87f31741f4fbdb7ac10f39f3615256d89
Josh Cunningham <josh@joshcanhelp.com>
2018-04-02 07:11:40 -0700

    Tinkering around with different image combinations

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-if [ -f "./wordpress/wp-config.php" ];
-then
-	echo "WordPress config file found."
-else
-	echo "WordPress config file not found. Installing..."
-	docker-compose exec --user www-data phpfpm wp core download
-	docker-compose exec --user www-data phpfpm wp core config --dbhost=mysql --dbname=wordpress --dbuser=root --dbpass=password
-fi
+docker-compose up -d
+#docker exec wplocaldocker_php-apache_1 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+#docker exec wplocaldocker_php-apache_1 chmod +x wp-cli.phar
+#docker exec wplocaldocker_php-apache_1 mv wp-cli.phar /usr/local/bin/wp
+#docker exec -u www-data wplocaldocker_php-apache_1 wp core download
+docker exec -u www-data wplocaldocker_php-apache_1 wp config create \
+    --dbname=wordpress \
+    --dbuser=wordpress\
+    --dbpass=password
+docker exec -u www-data wplocaldocker_php-apache_1 wp core install \
+    --url=http://localhost \
+    --title="Auth0 ♥ Docker" \
+    --admin_user=josh.cunningham@auth0.com \
+    --admin_email=josh.cunningham@auth0.com \
+    --admin_password="j2EwNZ{>goUN9aqFMirj"
+docker exec -u www-data wplocaldocker_php-apache_1 wp plugin install auth0
+docker exec -u www-data wplocaldocker_php-apache_1 wp plugin activate auth0
+docker-compose stop
